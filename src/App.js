fbb73f07cc03b7dcbc7907a7065149f84913053f
arafat-rifat <arafat15-12603@diu.edu.bd>
2024-04-07 02:52:21 +0600

    Creating New Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Navbar from "./Components/Navbar";
 import ListBox from "./Components/ListBox";
+import WatchedBox from "./Components/WatchedBox";
 
 const tempMovieData = [
   {
@@ -68,63 +69,12 @@ export default function App() {
       <main className="main">
         
         <ListBox movies={movies} setMovies={setMovies} isOpen1={isOpen1} setIsOpen1={setIsOpen1} />
+         
 
-        <div className="box">
-          <button
-            className="btn-toggle"
-            onClick={() => setIsOpen2((open) => !open)}
-          >
-            {isOpen2 ? "–" : "+"}
-          </button>
-          {isOpen2 && (
-            <>
-              <div className="summary">
-                <h2>Movies you watched</h2>
-                <div>
-                  <p>
-                    <span>#️⃣</span>
-                    <span>{watched.length} movies</span>
-                  </p>
-                  <p>
-                    <span>⭐️</span>
-                    <span>{avgImdbRating}</span>
-                  </p>
-                  <p>
-                    <span>🌟</span>
-                    <span>{avgUserRating}</span>
-                  </p>
-                  <p>
-                    <span>⏳</span>
-                    <span>{avgRuntime} min</span>
-                  </p>
-                </div>
-              </div>
-
-              <ul className="list">
-                {watched.map((movie) => (
-                  <li key={movie.imdbID}>
-                    <img src={movie.Poster} alt={`${movie.Title} poster`} />
-                    <h3>{movie.Title}</h3>
-                    <div>
-                      <p>
-                        <span>⭐️</span>
-                        <span>{movie.imdbRating}</span>
-                      </p>
-                      <p>
-                        <span>🌟</span>
-                        <span>{movie.userRating}</span>
-                      </p>
-                      <p>
-                        <span>⏳</span>
-                        <span>{movie.runtime} min</span>
-                      </p>
-                    </div>
-                  </li>
-                ))}
-              </ul>
-            </>
-          )}
-        </div>
+          <WatchedBox watched={watched} setWatched={setWatched} isOpen2={isOpen2} setIsOpen2={setIsOpen2} avgImdbRating={avgImdbRating} avgUserRating={avgUserRating} 
+          avgRuntime={avgRuntime}
+          />
+          
       </main>
     </>
   );
